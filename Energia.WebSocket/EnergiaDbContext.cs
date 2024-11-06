3edf9a0e066e2ca4425501eb5805d5ce569f16cf
fredso82 <fredso82@gmail.com>
2024-11-05 21:09:47 -0300

    Simulação de dispositivos

--- Energia.WebSocket/EnergiaDbContext.cs
+++ Energia.WebSocket/EnergiaDbContext.cs
@@ -4,11 +4,34 @@ namespace Energia.WebSocket
 {
     public class EnergiaDbContext : DbContext
     {
-        public DbSet<EnergiaDados> Consumos { get; set; }
+        public DbSet<Consumo> Consumos { get; set; }
+        public DbSet<Ambiente> Ambientes { get; set; }
+        public DbSet<TipoDispositivo> TiposDispositivo { get; set; }
+        public DbSet<Dispositivo> Dispositivos { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=energia_cosumo.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ambiente>().HasData(
+                new Ambiente { Id = 1, Nome = "Produção" },
+                new Ambiente { Id = 2, Nome = "Administrativo" }
+            );
+
+            modelBuilder.Entity<TipoDispositivo>().HasData(
+                new TipoDispositivo { Id = 1, Nome = "Energia" }
+            );
+
+            modelBuilder.Entity<Dispositivo>().HasData(
+                new Dispositivo { Id = 1, Nome = "Linha de Produção 1", AmbienteId = 1, TipoDispositivoId = 1 },
+                new Dispositivo { Id = 2, Nome = "Linha de Produção 2", AmbienteId = 1, TipoDispositivoId = 1 },
+                new Dispositivo { Id = 3, Nome = "Ar Condicionado", AmbienteId = 1, TipoDispositivoId = 1 },
+                new Dispositivo { Id = 4, Nome = "Ar Condicionado", AmbienteId = 2, TipoDispositivoId = 1 },
+                new Dispositivo { Id = 5, Nome = "Lâmpada", AmbienteId = 2, TipoDispositivoId = 1 }
+            );
         }
     }
 }
--- Energia.WebSocket/Program.cs
+++ Energia.WebSocket/Program.cs
@@ -1,5 +1,6 @@
 using Energia.WebSocket;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddDbContext<EnergiaDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
@@ -23,13 +25,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
 app.MapHub<EnergiaHub>("/energiaHub");
 
 app.MapGet("/consumos", async (EnergiaDbContext context) =>
 {
-    var consumos = await context.Consumos.ToListAsync();
+    var consumos = await context.Dispositivos
+        .Include(d => d.TipoDipositivo)
+        .Include(d => d.Ambiente)
+        .Include(d => d.Consumos).ToListAsync();
+
     return Results.Ok(consumos);
 }).WithName("consumos");
 
